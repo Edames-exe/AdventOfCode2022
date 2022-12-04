367d8a4da1d990c6d5c931ab11e0b873ff224a2b
James <ismay.james@gmail.com>
2022-12-04 12:32:53 +0000

    Day 1 complete

---
+++ Day1/Day1_part2.py
@@ -0,0 +1,14 @@
+import os
+os.chdir("d:\Advent_of_code2022\Day1")
+input = open("Day1_input.txt", "r")
+calories = 0
+elves = []
+for line in input:
+    if line != "\n":
+        calories += int(line)
+    else:
+        elves.append(calories)
+        calories = 0
+elves.sort(reverse=True)
+top3 = elves[0] + elves[1] + elves[2]
+print(top3)
