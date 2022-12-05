6bfaabefb29bfb8582eabd84c8b6af5264118582
James <Ismay.james@gmail.com>
2022-12-05 20:15:33 +0000

    It doesn't fucking work

--- Year-2022/Day5/Day5_part2.py
+++ Year-2022/Day5/Day5_part2.py
@@ -32,9 +32,32 @@ for command in range(10, len(lines), 1):
     num = int(splitLine[1])
     start = int(splitLine[3])
     end = int(splitLine[5])
+    temp = []
+    keepSpace = False
+    #print(splitLine)
+    if len(bigBoiArray[start-1]) < num:
+        num = len(bigBoiArray[start])
+        keepSpace = True
+
+    if keepSpace == True:
+        bigBoiArray[start-1].insert([start-1][0])
+        start += 1
+        num += 1
     for loop in range(1, num+1, 1):
-        bigBoiArray[end-1].append(bigBoiArray[start-1].pop())
+        temp.append(bigBoiArray[start-1])
+        bigBoiArray[start - 1].pop()
+    bigBoiArray[end-1].append(temp)
 
 print("------------")
 
 printArray(bigBoiArray)
+print(bigBoiArray[0][len(bigBoiArray[0])])
+print(bigBoiArray[1][len(bigBoiArray[1])])
+print(bigBoiArray[2][len(bigBoiArray[2])])
+print(bigBoiArray[3][len(bigBoiArray[3])])
+print(bigBoiArray[4][len(bigBoiArray[4])])
+print(bigBoiArray[5][len(bigBoiArray[5])])
+print(bigBoiArray[6][len(bigBoiArray[6])])
+print(bigBoiArray[7][len(bigBoiArray[7])])
+print(bigBoiArray[8][len(bigBoiArray[8])])
+
